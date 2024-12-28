0439e052de74e864542ebe913e22a5fa3f3c1f81
Leonardo Tagliati <leotagliati56@gmail.com>
2024-12-28 20:10:58 -0300

    tailwind

--- src/CovertLetter.jsx
+++ src/CovertLetter.jsx
@@ -6,7 +6,7 @@ function CovertLetter() {
             </div>
             <div className="containerParagraph" >
                 <img src="Images/alternativePhoto.gif" alt="" />
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, facere soluta. Itaque recusandae veniam at vero ab aspernatur? Reiciendis ipsum iste quasi magni officiis mollitia accusamus iure nostrum quidem rem!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, placeat architecto neque, deserunt laboriosam nemo unde expedita, nulla assumenda saepe aspernatur reprehenderit? Voluptas nihil omnis quisquam inventore asperiores eligendi nemo!</p>
+                <p><h2>Hello, i'm Leo!</h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, facere soluta. Itaque recusandae veniam at vero ab aspernatur? Reiciendis ipsum iste quasi magni officiis mollitia accusamus iure nostrum quidem rem!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, placeat architecto neque, deserunt laboriosam nemo unde expedita, nulla assumenda saepe aspernatur reprehenderit? Voluptas nihil omnis quisquam inventore asperiores eligendi nemo!</p>
             </div>
         </>
     );
